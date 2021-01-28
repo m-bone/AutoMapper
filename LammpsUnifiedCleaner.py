000df2009240b266bf2e82bc431ab76fb090d5bc
Matt Bone <matthew.bone@bristol.ac.uk>
2021-01-28 14:46:37 +0000

    UnifiedCleaner Main Update 1
    Now handling any number of data files
    Tidiest implementation with classes and neat funcs

--- LammpsUnifiedCleaner.py
+++ LammpsUnifiedCleaner.py
@@ -1,7 +1,11 @@
 import os
 import re
 import sys
+from itertools import combinations_with_replacement
 from LammpsTreatmentFuncs import clean_data, clean_settings, find_sections, get_data, add_section_keyword
+
+# TO DO - Extras
+# Remove lj/cut... for settings without hbonding. Would still need to fix hybrid in init
 
 # ASSUMPTIONS
 # Box size is identical between files
@@ -25,46 +29,42 @@ from LammpsTreatmentFuncs import clean_data, clean_settings, find_sections, get_
 #   y1 y2 ylo yhi
 #   z1 z2 zlo zhi
 
-def first_file_unifier(directory, dataFile1, dataFile2, settingsFile):
-    # Go to file directory
-    os.chdir(directory)
-
-    # Load dataFile into python as a list of lists
-    with open(dataFile1, 'r') as f:
-        data1 = f.readlines()
-
-    # Tidy data1
-    data1 = clean_data(data1)
-
-
-    # Load dataFile into python as a list of lists
-    with open(dataFile2, 'r') as f:
-        data2 = f.readlines()
-
-    # Tidy data2
-    data2 = clean_data(data2)
-
-
-    # Load dataFile into python as a list of lists
-    with open(settingsFile, 'r') as f:
-        settings = f.readlines()
-    
-    # Tidy settings
-    settings = clean_settings(settings)
-
+def file_unifier(directory, dataList, settingsFile):
     # Class for handling Lammps data
     class Data:
         def __init__(self, data):
             self.data = data
             self.sectionIndexList = find_sections(self.data)
+
+            # Section data
             self.atoms = get_data('Atoms', self.data, self.sectionIndexList)
             self.masses = get_data('Masses', self.data, self.sectionIndexList)
+            self.bonds = get_data('Bonds', self.data, self.sectionIndexList)
+            self.angles = get_data('Angles', self.data, self.sectionIndexList)
+            self.dihedrals = get_data('Dihedrals', self.data, self.sectionIndexList)
+            self.impropers = get_data('Impropers', self.data, self.sectionIndexList)
         
         def get_atom_types(self):
             atom_types = {atom[2] for atom in self.atoms}
             return atom_types
         
-        def get_mass_types(self, unioned_atom_types):
+        def get_bond_types(self):
+            bond_types = {bond[1] for bond in self.bonds}
+            return bond_types
+
+        def get_angle_types(self):
+            angle_types = {angle[1] for angle in self.angles}
+            return angle_types
+
+        def get_dihedral_types(self):
+            dihedral_types = {dihedral[1] for dihedral in self.dihedrals}
+            return dihedral_types
+
+        def get_improper_types(self):
+            improper_types = {improper[1] for improper in self.impropers}
+            return improper_types
+
+        def change_mass_types(self, unioned_atom_types):
             # Get masses that are using in atoms
             valid_masses = [mass for mass in self.masses if mass[0] in unioned_atom_types]
             # Get atom types
@@ -81,28 +81,69 @@ def first_file_unifier(directory, dataFile1, dataFile2, settingsFile):
             
             return valid_masses, mass_change_dict
         
-        def update_atoms(self, mass_change_dict):
+        def change_atoms(self, mass_change_dict):
             for atomList in self.atoms:
                 atomList[2] = mass_change_dict[atomList[2]]
 
-    # Initialise classes for Lammps data
-    Lammps1 = Data(data1)
-    Lammps2 = Data(data2)
+    # Go to file directory
+    os.chdir(directory)
 
-    # Union sets and create sorted list
-    atomTypes = Lammps1.get_atom_types().union(Lammps2.get_atom_types())
-    atomTypes = sorted(atomTypes)
+    # Load files from dataList, tidy and initialise class object
+    lammpsData = []
+    for dataFile in dataList:
+        with open(dataFile, 'r') as f:
+            data = f.readlines()
+
+        # Tidy data
+        data = clean_data(data)
+
+        # Initialise data class
+        data = Data(data)
+        lammpsData.append(data)
+
+
+    # Load dataFile into python as a list of lists
+    with open(settingsFile, 'r') as f:
+        settings = f.readlines()
+    
+    # Tidy settings
+    settings = clean_settings(settings)
+
+    def union_types(typeAttr, lammpsData=lammpsData):
+        lammpsTypes = []
+        for data in lammpsData:
+            # Get attribute for determining types
+            func = getattr(data, typeAttr)
+            # Run function and append to list
+            lammpsTypes.append(func())
+
+        # Union sets to remove duplicates and sort into numerical order list 
+        types = sorted(set().union(*lammpsTypes))
+        numTypes = len(types)
+        
+        print(f'{typeAttr}\n Types: {types}\n Count: {numTypes}')
+
+        return types, numTypes
+
+    # Union sets and create sorted list for each type
+    atomTypes, numAtomTypes = union_types('get_atom_types')
+    bondypes, numBondTypes = union_types('get_bond_types')
+    angleTypes, numAngleTypes = union_types('get_angle_types')
+    dihedralTypes, numdihedralTypes = union_types('get_dihedral_types')
+    improperTypes, numImproperTypes = union_types('get_improper_types')
 
     # Build new masses section - eliminate unused masses
-    processedMass, massDict = Lammps1.get_mass_types(atomTypes)
+    processedMass, massDict = lammpsData[0].change_mass_types(atomTypes)
     processedMass = add_section_keyword('Masses', processedMass)
 
     # Update atom types and get atoms sections
-    Lammps1.update_atoms(massDict)
-    Lammps2.update_atoms(massDict)
-    processedAtoms1 = add_section_keyword('Atoms', Lammps1.atoms)
-    processedAtoms2 = add_section_keyword('Atoms', Lammps2.atoms)
+    for data in lammpsData:
+        data.change_atoms(massDict)
+    processedAtoms = [add_section_keyword('Atoms', data.atoms) for data in lammpsData]
+
+    # Update pair coeffs
+    originalPairTuples = list(combinations_with_replacement(atomTypes, 2))
 
     print()
 
-first_file_unifier('/home/matt/Documents/Oct20-Dec20/Bonding_Test/React_Test/PythonCode', 'pre-system.data', 'post-system.data', 'system.in.settings')
+file_unifier('/home/matt/Documents/Bond_React_Python/', ['pre-system.data', 'post-system.data'], 'system.in.settings')
