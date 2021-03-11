b1fb9c58827db74859eb0ad39f5b5675b6cc49ce
Matt Bone <matthew.bone@bristol.ac.uk>
2021-03-11 09:42:02 +0000

    Ambiguous Pairs and Testing
    Initial attempt at handling ambiguous pairs, to be developed further
    Testing framework for two examples

--- AtomMapping.py
+++ AtomMapping.py
@@ -1,5 +1,7 @@
 import os
+import math
 import numpy as np
+from natsort import natsorted
 from sklearn.metrics import mean_absolute_error
 
 from BondDistanceMatrix import bond_distance_matrix
@@ -14,45 +16,83 @@ PRE_BONDING_ATOMS = ['28', '62']
 POST_BONDING_ATOMS = ['32', '15']
 POST_MAJOR_MOVED_ATOMS = ['33']
 POST_MINOR_MOVED_ATOMS = ['16']
-_WEIGHT_COEFF = 0.0
 
-# Get elements of atom IDs for pre and post molecules
-preElements = element_atomID_dict(DATA_DIR, PRE_FILE_NAME, ELEMENT_BY_TYPE)
-postElements = element_atomID_dict(DATA_DIR, POST_FILE_NAME, ELEMENT_BY_TYPE)
 
-# Get atomIDs - using existing function for now 
-preAtomIDs = get_atomIDs(DATA_DIR, PRE_FILE_NAME)
-postAtomIDs = get_atomIDs(DATA_DIR, POST_FILE_NAME)
+def atom_mapping(DATA_DIR, PRE_FILE_NAME, POST_FILE_NAME, ELEMENT_BY_TYPE, PRE_BONDING_ATOMS, POST_BONDING_ATOMS, POST_MAJOR_MOVED_ATOMS, POST_MINOR_MOVED_ATOMS):
+    _WEIGHT_COEFF = 0.0
+    # Get elements of atom IDs for pre and post molecules
+    preElements = element_atomID_dict(DATA_DIR, PRE_FILE_NAME, ELEMENT_BY_TYPE)
+    postElements = element_atomID_dict(DATA_DIR, POST_FILE_NAME, ELEMENT_BY_TYPE)
 
-# Calculate bond distance matrices for pre and post molecule
-preBondDistMat = bond_distance_matrix(DATA_DIR, PRE_FILE_NAME, PRE_BONDING_ATOMS)
-postBondDistMat = bond_distance_matrix(DATA_DIR, POST_FILE_NAME, POST_BONDING_ATOMS) 
+    # Get atomIDs - using existing function for now 
+    preAtomIDs = get_atomIDs(DATA_DIR, PRE_FILE_NAME)
+    postAtomIDs = get_atomIDs(DATA_DIR, POST_FILE_NAME)
 
-# Set value for hydrogen that moves to epoxide ring to zero - this will be automated / user supplied info in the future
-for atom in POST_MAJOR_MOVED_ATOMS:
-    atomIndex = postAtomIDs.index(atom)
-    for index, atomRow in enumerate(postBondDistMat):
-        postBondDistMat[index][atomIndex] = 0.0
+    # Calculate bond distance matrices for pre and post molecule
+    preBondDistMat = bond_distance_matrix(DATA_DIR, PRE_FILE_NAME, PRE_BONDING_ATOMS)
+    postBondDistMat = bond_distance_matrix(DATA_DIR, POST_FILE_NAME, POST_BONDING_ATOMS) 
 
-# Sample weight matrix - lower weight for atoms with significant movement
-sampleWeights = np.ones(len(postAtomIDs))
-for atom in POST_MINOR_MOVED_ATOMS:
-    atomIndex = postAtomIDs.index(atom)
-    sampleWeights[atomIndex] = _WEIGHT_COEFF
+    # Set value for hydrogen that moves to epoxide ring to zero - this will be automated / user supplied info in the future
+    for atom in POST_MAJOR_MOVED_ATOMS:
+        atomIndex = postAtomIDs.index(atom)
+        for index, _ in enumerate(postBondDistMat):
+            postBondDistMat[index][atomIndex] = 0.0
 
-mappedIDList = []
-for searchIndex, searchRow in enumerate(preBondDistMat):
-    # Shortcircuit this search if search atom is a bonding atom
-    if preAtomIDs[searchIndex] in PRE_BONDING_ATOMS:
-        bondingIndex = PRE_BONDING_ATOMS.index(preAtomIDs[searchIndex])
-        bondingPostAtomID = POST_BONDING_ATOMS[bondingIndex]
-        mappedIDList.append([preAtomIDs[searchIndex], bondingPostAtomID])
+    # Sample weight matrix - lower weight for atoms with significant movement
+    sampleWeights = np.ones(len(postAtomIDs))
+    for atom in POST_MINOR_MOVED_ATOMS:
+        atomIndex = postAtomIDs.index(atom)
+        sampleWeights[atomIndex] = _WEIGHT_COEFF
 
-    else:
-        # Sort search row arrays from smallest to largest
-        searchRowIndex = np.argsort(searchRow)
-        searchRowSorted = np.take_along_axis(searchRow, searchRowIndex, axis=0)
+    mappedIDList = []
+    mappedPostAtomsIndex = []
+    for searchIndex, searchRow in enumerate(preBondDistMat):
+        # Shortcircuit this search if search atom is a bonding atom
+        if preAtomIDs[searchIndex] in PRE_BONDING_ATOMS:
+            bondingIndex = PRE_BONDING_ATOMS.index(preAtomIDs[searchIndex])
+            bondingPostAtomID = POST_BONDING_ATOMS[bondingIndex]
+            mappedIDList.append([preAtomIDs[searchIndex], bondingPostAtomID])
+
+        else:
+            # Sort search row arrays from smallest to largest
+            searchRowIndex = np.argsort(searchRow)
+            searchRowSorted = np.take_along_axis(searchRow, searchRowIndex, axis=0)
+            
+            distDifference = []
+            for row in postBondDistMat:
+                # Sort row arrays from smallest to largest
+                rowIndex = np.argsort(row)
+                rowSorted = np.take_along_axis(row, rowIndex, axis=0)
+                
+                # Sort sample weight matrix the same as row
+                sampleWeightsSorted = np.take_along_axis(sampleWeights, rowIndex, axis=0)
+
+                # MAE
+                finalVal = mean_absolute_error(searchRowSorted, rowSorted, sample_weight=sampleWeightsSorted)
+
+                # Append - abs to get smallest value closest to zero
+                distDifference.append(abs(finalVal))
+
+            mappedPreAtomID, mappedPostAtomID, postAtomIDIndex = element_validation(preAtomIDs[searchIndex], postAtomIDs, distDifference, preElements, postElements, POST_BONDING_ATOMS)
+
+            mappedIDList.append([mappedPreAtomID, mappedPostAtomID])
+            mappedPostAtomsIndex.append(postAtomIDIndex)
+
+    # Ambiguous Atom Group Processing
+    # Gather all the pairs
+    mappedPostAtomIDs = [val[1] for val in mappedIDList]
+    repeatedPostIDSet = natsorted(set([val for val in mappedPostAtomIDs if mappedPostAtomIDs.count(val) > 1]))
+    repeatedIndexes = [postAtomIDs.index(ID) for ID in repeatedPostIDSet]
+
+    ambiguousGroupPairs = []
+    # Loop through all post atoms to find similar
+    for index in repeatedIndexes:
+        matchArray = postBondDistMat[index]
         
+        # Sort search row arrays from smallest to largest
+        searchRowIndex = np.argsort(matchArray)
+        searchRowSorted = np.take_along_axis(matchArray, searchRowIndex, axis=0)
+            
         distDifference = []
         for row in postBondDistMat:
             # Sort row arrays from smallest to largest
@@ -68,26 +108,23 @@ for searchIndex, searchRow in enumerate(preBondDistMat):
             # Append - abs to get smallest value closest to zero
             distDifference.append(abs(finalVal))
 
-        mappedPreAtomID, mappedPostAtomID = element_validation(preAtomIDs[searchIndex], postAtomIDs, distDifference, preElements, postElements)
+        # Set repeatedIndex value to nan as it will always be zero
+        distDifference[index] = math.nan
+        _, smallestIndex = min((val, idx) for (idx, val) in enumerate(distDifference))
 
-        mappedIDList.append([mappedPreAtomID, mappedPostAtomID])
+        ambiguousGroupPairs.append([postAtomIDs[index], postAtomIDs[smallestIndex]])
+        # print(f'Atom {postAtomIDs[index]} is paired to atom {postAtomIDs[smallestIndex]}')
 
+    # Update mappedIDList based on the ambiguousGroupPairs values
+    # Interestingly, mappedIDList can be updated with the iterator, but ambiguousGroupPairs needs to be deleted with the index value
+    for mappedID in mappedIDList:
+        if mappedID[1] in repeatedPostIDSet: # If mappedPostAtomID is one that is repeated
+            for index, groupPair in enumerate(ambiguousGroupPairs):
+                if groupPair[0] == mappedID[1]: # If groupPair is a matching PostAtomID
+                    mappedID[1] = groupPair[1]
+                    del ambiguousGroupPairs[index]
+                    break
 
+    return mappedIDList
+# This needs to include a check to make sure it's not updating the atom to an ID already assigned - might be best to have an unassigned-postAtomIDList
 
-# Print test report
-for mappedPair in mappedIDList:
-    print(f'Atom {mappedPair[0]} is mapped to atom {mappedPair[1]}')
-
-correctPostAtomIDs = [['38'], ['39'], ['35'], ['41', '42'], ['42', '41'], ['32'], ['16'], ['5', '36'], ['36', '5'], ['37'], ['6', '9'], ['4'], ['1', '3'], ['3', '1'], ['9', '6'], ['17', '23'], ['23', '17'], ['15'], ['33', '34'], ['34', '33']]
-totalAtoms = len(correctPostAtomIDs)
-correctAtoms = 0
-incorrectPreAtomsList = []
-for index, atom in enumerate(mappedIDList):
-    if atom[1] in correctPostAtomIDs[index]:
-        correctAtoms += 1
-    else:
-        incorrectPreAtomsList.append(atom[0])
-
-print(f'Test Results: Weight coeff is {_WEIGHT_COEFF}')
-print(f'Correct atoms: {correctAtoms}. Accuracy: {round(correctAtoms / totalAtoms * 100, 1)}%')
-print(f'Incorrect premolecule atomIDs: {incorrectPreAtomsList}')
